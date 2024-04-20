7500660fa3c0716065e160e75d6660520ff7e383
stan <nzegwustanley@gmail.com>
2024-04-20 13:10:02 +0100

    new commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,12 @@
-// import Footer from "@/components/shared/Footer"
-// import Header from "@/components/shared/Header"
+import { Footer } from "@/components/shared/Footer";
+import { Header } from "@/components/shared/Header";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col">
-      {/* <Header /> */}
+      <Header />
       <main className="flex-1">{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
